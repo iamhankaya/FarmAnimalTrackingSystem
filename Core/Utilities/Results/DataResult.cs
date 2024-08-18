5a21d2a1ca16c96951a6bff3b43c554e0cf21ba2
Metehan KAYA <210601001@bakircay.edu.tr>
2024-08-19 00:13:55 +0300

    Core added

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Utilities.Results
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+        public DataResult(T data, bool success, string message) : base(success, message)
+        {
+            this.data = data;
+        }
+        public DataResult(T data, bool success) : base(success)
+        {
+            this.data = data;
+        }
+
+        public T data { get; }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool IsSuccess, string message) : this(IsSuccess)
+        {
+            this.Message = message;
+        }
+        public Result(bool IsSuccess)
+        {
+            this.IsSuccess = IsSuccess;
+        }
+        public bool IsSuccess { get; }
+
+        public string Message { get; }
+    }
+}
